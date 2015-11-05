86bee26ba25dcce445fd763fd67b68c2a3535ebf
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2015-11-05 10:53:56 +0100

    Make name and key_name required parameters, no defaults.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "name" { default = "rtmo" }
-variable "key_name" { default = "rtmo-key" }
 variable "vpc_cidr" { default = "10.139.0.0/16" }
 variable "subnet_cidr" { default = "10.139.101.0/24" }
+variable "name" {}
+variable "key_name" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "atlas_username" {}
